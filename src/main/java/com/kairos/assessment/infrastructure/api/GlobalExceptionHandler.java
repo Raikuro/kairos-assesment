eb0ef21a465486e8cde9ee8b5a5062b28a4cd286
Raikuro <jugragu22@gmail.com>
2024-05-07 00:45:41 +0200

    feature: MethodArgumentTypeMismatchException and MissingServletRequestParameterException are now handled.

--- src/main/java/com/kairos/assessment/infrastructure/api/GlobalExceptionHandler.java
+++ src/main/java/com/kairos/assessment/infrastructure/api/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Log4j2
 @RestControllerAdvice
@@ -14,15 +16,29 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity handleNotFoundException(NotFoundException ex) {
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
         log.info(ex);
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity handleException(NotFoundException ex) {
+    public ResponseEntity<String> handleException(NotFoundException ex) {
         log.info(ex);
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleException(MethodArgumentTypeMismatchException ex) {
+        log.info(ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleException(MissingServletRequestParameterException ex) {
+        log.info(ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
